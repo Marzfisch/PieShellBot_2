25e043eef04a46f7a9ac97fb1abda2a6e26f591e
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 15:24:34 -0700

    Added isOpen and isClosed booleans

--- src/com/archerfish/api/myBank.java
+++ src/com/archerfish/api/myBank.java
@@ -1,4 +1,10 @@
 package com.archerfish.api;
 
+import org.dreambot.api.methods.container.impl.bank.Bank;
+
 public class myBank {
+
+    public static boolean isOpen = Bank.isOpen();
+    public static boolean isClosed = !Bank.isOpen();
+
 }
