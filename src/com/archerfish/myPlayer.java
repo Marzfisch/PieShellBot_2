dfca3d3c05a1cabe5a3416d7b6988f650cb56484
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 14:58:19 -0700

    Added 'myPlayer' class, with local player attribute

---
+++ src/com/archerfish/myPlayer.java
@@ -0,0 +1,10 @@
+package com.archerfish;
+
+import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.wrappers.interactive.Player;
+
+public class myPlayer {
+
+    public static Player player = Players.getLocal();
+
+}
