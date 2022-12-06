dad0d9a312139048afd42c2f60eb4de52af6c5d1
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 15:51:42 -0700

    Addded two initialBankCheckLeafs

---
+++ src/com/archerfish/behavior/initialBankCheckLeafs/InitialRunToBankLeaf.java
@@ -0,0 +1,18 @@
+package com.archerfish.behavior.initialBankCheckLeafs;
+
+import org.dreambot.Main;
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.framework.Leaf;
+
+public class InitialRunToBankLeaf extends Leaf<Main> {
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public int onLoop() {
+        return (int) Calculations.nextGaussianRandom(350, 250);
+    }
+}
---
+++ src/com/archerfish/behavior/initialBankCheckLeafs/initialBankCheckLeaf.java
@@ -0,0 +1,20 @@
+package com.archerfish.behavior.initialBankCheckLeafs;
+
+import org.dreambot.Main;
+import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.bank.BankLocation;
+import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.framework.Leaf;
+
+public class initialBankCheckLeaf extends Leaf<Main> {
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public int onLoop() {
+        return (int) Calculations.nextGaussianRandom(350, 250);
+    }
+}
