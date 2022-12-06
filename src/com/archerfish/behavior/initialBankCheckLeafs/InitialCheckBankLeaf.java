c3341805435cbec27548c8c48e7d5c4613e04599
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 16:18:10 -0700

    First functional commit

--- src/com/archerfish/behavior/initialBankCheckLeafs/InitialCheckBankLeaf.java
+++ src/com/archerfish/behavior/initialBankCheckLeafs/InitialCheckBankLeaf.java
@@ -1,18 +1,26 @@
 package com.archerfish.behavior.initialBankCheckLeafs;
 
+import com.archerfish.api.myPlayer;
+import com.archerfish.behavior._DynamicVariables;
 import org.dreambot.Main;
 import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.bank.Bank;
+import org.dreambot.api.utilities.Sleep;
 import org.dreambot.framework.Leaf;
 
 public class InitialCheckBankLeaf extends Leaf<Main> {
 
     @Override
     public boolean isValid() {
-        return true;
+        return Bank.getClosestBankLocation().getArea(4).contains(myPlayer.player.getTile());
     }
 
     @Override
     public int onLoop() {
+        Bank.open();
+        Sleep.sleepUntil(Bank::isOpen, 15_000);
+        Sleep.sleep(10_000);
+        _DynamicVariables.initiallyCheckedBank = true;
         return (int) Calculations.nextGaussianRandom(350, 250);
     }
 }
--- src/com/archerfish/behavior/initialBankCheckLeafs/InitialRunToBankLeaf.java
+++ src/com/archerfish/behavior/initialBankCheckLeafs/InitialRunToBankLeaf.java
@@ -1,18 +1,23 @@
 package com.archerfish.behavior.initialBankCheckLeafs;
 
+import com.archerfish.api.myPlayer;
 import org.dreambot.Main;
 import org.dreambot.api.methods.Calculations;
+import org.dreambot.api.methods.container.impl.bank.Bank;
 import org.dreambot.framework.Leaf;
 
 public class InitialRunToBankLeaf extends Leaf<Main> {
 
+    // TODO: Make custom handler for messy code, and fix the 'radius' arg hardcode
     @Override
     public boolean isValid() {
-        return true;
+        return !Bank.getClosestBankLocation().getArea(4).contains(myPlayer.player.getTile());
     }
 
+    // TODO: You typed some stuff twice.... fix that, and the 'radius' arg hardcode
     @Override
     public int onLoop() {
+        myPlayer.walk(Bank.getClosestBankLocation().getArea(4));
         return (int) Calculations.nextGaussianRandom(350, 250);
     }
 }
