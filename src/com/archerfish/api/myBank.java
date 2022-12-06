45e13e12b5e28555f00a32472595fe08fdfb331d
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 16:38:17 -0700

    Added bankedCoins attribute, assigned to 0 until counted

--- src/com/archerfish/api/myBank.java
+++ src/com/archerfish/api/myBank.java
@@ -4,7 +4,10 @@ import org.dreambot.api.methods.container.impl.bank.Bank;
 
 public class myBank {
 
+    public static int bankedCoins = 0;
+
     public static boolean isOpen = Bank.isOpen();
     public static boolean isClosed = !Bank.isOpen();
+
 
 }
