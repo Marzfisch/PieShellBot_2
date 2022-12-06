9dfd806747ad1808f1b53aa75ff95def6d8446d8
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 16:18:51 -0700

    Added method of proper implementation of the walking API

--- src/com/archerfish/api/myPlayer.java
+++ src/com/archerfish/api/myPlayer.java
@@ -1,7 +1,10 @@
 package com.archerfish.api;
 
+import org.dreambot.api.methods.Calculations;
 import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.interactive.Players;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.wrappers.interactive.Player;
 
 public class myPlayer {
@@ -10,5 +13,17 @@ public class myPlayer {
 
     public static boolean inventoryIsFull = Inventory.isFull();
     public static boolean inventoryIsEmpty = Inventory.isEmpty();
+
+
+    /**
+     * Proper walking API implementation
+     * @param area Area to walk to
+     */
+    public static void walk(Area area) {
+        if (Walking.shouldWalk(Calculations.random(3, 12))) {
+            Walking.walk(area);
+        }
+    }
+
 
 }
