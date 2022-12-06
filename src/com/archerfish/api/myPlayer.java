fbda3a3a0c2bd097fd8d9d06d06ca4b7b41aaa0b
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 15:30:26 -0700

    Added inventoryIsFull and inventoryIsEmpty attributes

--- src/com/archerfish/api/myPlayer.java
+++ src/com/archerfish/api/myPlayer.java
@@ -1,10 +1,14 @@
 package com.archerfish.api;
 
+import org.dreambot.api.methods.container.impl.Inventory;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.wrappers.interactive.Player;
 
 public class myPlayer {
 
     public static Player player = Players.getLocal();
+
+    public static boolean inventoryIsFull = Inventory.isFull();
+    public static boolean inventoryIsEmpty = Inventory.isEmpty();
 
 }
