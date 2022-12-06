a8262f13d95040a0dca46ac62386d7e8fc378276
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 15:46:44 -0700

    Renamed Branch

--- src/com/archerfish/behavior/InitialBankCheckBranch.java
+++ src/com/archerfish/behavior/InitialBankCheckBranch.java
@@ -3,7 +3,7 @@ package com.archerfish.behavior;
 import org.dreambot.Main;
 import org.dreambot.framework.Root;
 
-public class InitialBankCheck extends Root<Main> {
+public class InitialBankCheckBranch extends Root<Main> {
     @Override
     public boolean isValid() {
         return true;
