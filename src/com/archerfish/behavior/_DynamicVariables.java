bcb9ce1445dfad5b14309b587760eb8e9f2ebbb8
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 16:36:48 -0700

    Added certain banked item counts, initialized to 0 temporarily

--- src/com/archerfish/behavior/_DynamicVariables.java
+++ src/com/archerfish/behavior/_DynamicVariables.java
@@ -4,4 +4,9 @@ public class _DynamicVariables {
 
     public static boolean initiallyCheckedBank = false;
 
+    public static int bankedSoftClay = 0;
+    public static int bankedClay = 0;
+    public static int bankedJugs = 0;
+    public static int bankedJugsOfWater = 0;
+
 }
