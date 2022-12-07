ad4f3719834ac52db411e22a1cf7ce8d6c1f3199
MallsArcherfish <marz.fisch@gmail.com>
2022-12-06 21:17:59 -0700

    Moved initiallyCheckedBank to Constant variables, and added treeGrindingArea

--- src/com/archerfish/behavior/_ConstantVariables.java
+++ src/com/archerfish/behavior/_ConstantVariables.java
@@ -1,4 +1,23 @@
 package com.archerfish.behavior;
 
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.map.Tile;
+
 public class _ConstantVariables {
+
+
+    public static Area treeGrindingArea = new Area(
+            new Tile(3162, 3466, 0),
+            new Tile(3145, 3466, 0),
+            new Tile(3144, 3457, 0),
+            new Tile(3143, 3454, 0),
+            new Tile(3148, 3452, 0),
+            new Tile(3150, 3447, 0),
+            new Tile(3156, 3444, 0),
+            new Tile(3163, 3450, 0),
+            new Tile(3173, 3450, 0),
+            new Tile(3173, 3456, 0),
+            new Tile(3164, 3460, 0));
+
+    public static boolean initiallyCheckedBank = false;
 }
